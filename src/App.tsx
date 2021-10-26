52a80f7142ece850a7f21eff534a5e35056cd543
annasun919 <yuval_sun@yahoo.com.hk>
2021-10-26 11:10:51 +0200

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Note{
+  id:string;
+  note: string;
+}
+
+const App : React.FC=()=>{
+const [state, setstate] = useState<Note[]>([])
+
+function addnote(){
+
+}
+
+  return<>
+  <form onSubmit={addnote}>
+  <input type="text" placeholder="Please enter note"/>
+  <button type="submit" >add note</button>
+ </form>
+  </>
 }
 
 export default App;
