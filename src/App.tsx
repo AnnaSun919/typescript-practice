2d6daaa42d20b7086139683ede5555dd95dfc524
annasun919 <yuval_sun@yahoo.com.hk>
2021-11-23 14:09:43 +0100

    add readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,48 @@
 import React,{useState, useRef} from 'react';
 import './App.css';
 
-type drag = {
-  onDrag: (event: React.FormEvent, index:string) => void;
-};
-
-//status of the notes
-enum planStatus {
-  Plan,
-  Processing,
-  Finished
-  
-}
 
 interface Note{
   id:string;
   plan: string;
+  projectstate: string;
 }
 
 const App : React.FC=()=>{
   let textInputRef = useRef<HTMLInputElement>(null);
 const [state, setstate] = useState<Note[]>([])
+const[movingItem, setMovingItem] =useState()
+const [show, setshow] = useState<Note[]>([])
+
 
 function addnote(event : React.FormEvent){
   event.preventDefault();
-  setstate(prevPlan => [   { id: Math.random().toString(), plan: textInputRef.current!.value },
+  setstate(prevPlan => [   { id: Math.random().toString(), plan: textInputRef.current!.value , projectstate: "plan"},
     ...prevPlan
   ]);
 
 }
 
-function ondragstart(event:any){
-
-  event
-  .dataTransfer
-  .setData('text/plain', event.target.id);
-
-  event
-  .currentTarget
-  .style
-  .backgroundColor = 'yellow';
- 
- 
- 
-
-
+function onDragStart(event:any){
+  setMovingItem(event.target.id)
 };
+
+
+function onDragOver(event:React.FormEvent) {
+  event.preventDefault();
+}
+
+function onDrop(event:any){
+  const draggableElement = document!.getElementById!(movingItem!) as HTMLInputElement
+
+  setshow(prevPlan => [   { id: draggableElement.id, plan: draggableElement.innerText , projectstate: event.target.id},
+  ...prevPlan
+]);
+
+  
+
+
+}
 
 const reset = (event: React.FormEvent) =>{
   event.preventDefault()
@@ -63,36 +60,34 @@ const reset = (event: React.FormEvent) =>{
  </form>
  
 
- <div className="notesContainer">{state.map((plan,index)=> <div id={plan.id} draggable="true" onDrag={(event) =>{ondragstart(event)}} className="notes" key={index}>{plan.plan}</div>)}</div>
+ <div className="notesContainer">{state.map((plan,index)=> <div id={plan.id} draggable="true" onDrag={(event) =>{onDragStart(event)}} className="notes" key={index}>{plan.plan}</div>)}</div>
  </div>
 
  <div className="category">
- <table >
-  <tr >
-    <th >Plan</th>
-  </tr>
-  <tr>
-  <th draggable="true" className="Plan" 
-  ></th>
-  </tr>
+
+ <table draggable="true" id="Plan"  onDragOver={(event) =>{onDragOver(event)}}
+  onDrop={(event) =>{onDrop(event)}} >
+    <th>Plan</th>
+  
+  {show.map((plan)=><>{plan.projectstate === "Plan"? <div className="notes">{plan.plan}</div> : "" }      </>)}
+ 
 </table>
 
-<table >
-  <tr >
+
+<table draggable="true" id="Processing"  onDragOver={(event) =>{onDragOver(event)}}
+  onDrop={(event) =>{onDrop(event)}} >
     <th>Processing</th>
-  </tr>
-  <tr>
-  <th draggable="true" className="Processing"></th>
-  </tr>
+  
+  {show.map((plan)=><>{plan.projectstate === "Processing"? <div className="notes">{plan.plan}</div> : "" }      </>)}
+ 
 </table>
 
-<table >
-  <tr >
+<table draggable="true" id="Finished"  onDragOver={(event) =>{onDragOver(event)}}
+  onDrop={(event) =>{onDrop(event)}} >
     <th>Finished</th>
-  </tr>
-  <tr>
-  <th draggable="true" className="Finished"></th>
-  </tr>
+  
+  {show.map((plan)=><>{plan.projectstate === "Finished"? <div className="notes">{plan.plan}</div> : "" }      </>)}
+ 
 </table>
  </div>
  </div>
